06a394450c73a54edefedd7e80c0ffb0ed03b1e6
Jijunjiang <jiangjijun1994@gmail.com>
2018-05-18 00:46:41 -0500

    add embedding feature test case

--- table-entity-linker-test/src/main/java/edu/northwestern/websail/tabel/unitTest/TestEmbedding.java
+++ table-entity-linker-test/src/main/java/edu/northwestern/websail/tabel/unitTest/TestEmbedding.java
@@ -1,11 +1,18 @@
 package edu.northwestern.websail.tabel.unitTest;
 
 import Jama.Matrix;
+import edu.northwestern.websail.datastructure.trie.impl.w2cSQL.W2CSQLTrie;
 import edu.northwestern.websail.tabel.Embeddings.Embeddings;
-
+import edu.northwestern.websail.tabel.io.ResourceLoader;
+import edu.northwestern.websail.tabel.io.TableDataReader;
+import edu.northwestern.websail.tabel.model.*;
+import edu.northwestern.websail.tabel.train.ModelTrainingDataExtractor;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Created by apple on 15/05/2018.
@@ -14,7 +21,7 @@ import java.util.Map;
 
 public class TestEmbedding {
 
-    static public void main(String[] args) {
+    static public void main(String[] args) throws Exception{
         Embeddings embObj = new Embeddings();
         embObj.loadEmbedding();
         HashMap<String, double[]> map = embObj.embeddingVectorsMap_c;
@@ -36,5 +43,44 @@ public class TestEmbedding {
         double res3 = embObj.cosineSimilarity(m1, m2);
         System.out.println(res == res2 &&  res == res3 ? "test1 pass" : "test1 fail");
 
+
+
+        ArrayList<WtTable> tb = TableDataReader.loadTableFromResource("/test.json");
+        WtTable t1 = tb.get(0);
+        ArrayList<Mention> mentions = extractMention(t1);
+        //W2CSQLTrie trie = ResourceLoader.loadTrie();
+        Mention mention = mentions.get(0);
+        Candidate candidate = mention.candidates.get(0);
+        for (Candidate c : mention.candidates) {
+            System.out.println("candidate:" + "  " + c.toString());
+            System.out.println("col similarity:" + "    " + Double.toString(embObj.AverageColSimilarity(mention, c, t1)));
+            System.out.println("row similarity:" + "    " + Double.toString(embObj.AverageRowSimilarity(mention, c, t1)));
+            System.out.println("vector similarity:" + "    " + Double.toString(embObj.vectorFeatureOfSubjectColumn(mention, c, t1)));
+        }
+
+    }
+
+    public static ArrayList<Mention> extractMention(WtTable table) {
+        ArrayList<Mention> mentions = new ArrayList<Mention>();
+        int row = table.numDataRows;
+        int col = table.numCols;
+        for (int i=0; i<row; i++)
+            for (int j=0; j<col; j++) {
+                WikiCell c = table.tableData[i][j];
+                if (c.text == null)
+                    continue;
+
+                if (c.surfaceLinks != null && c.surfaceLinks.size() > 0) {
+                    Mention m = new Mention(c.text, 1, 1);
+                    m.cellRow = i;
+                    m.cellCol = j;
+                    WikiLink link = c.surfaceLinks.get(0);
+                    Candidate candidate = new Candidate(m, link.target.title, link.target.id);
+                    candidate.setIsMentionExact(true);
+                    m.candidates.add(candidate);
+                    mentions.add(m);
+                }
+            }
+        return mentions;
     }
 }

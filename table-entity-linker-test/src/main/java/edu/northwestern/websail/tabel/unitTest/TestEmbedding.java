d5760b0b535f3f7655b259dda04aaab6ae26e940
Jijunjiang <jiangjijun1994@gmail.com>
2018-05-17 21:18:26 -0500

    xxx

--- table-entity-linker-test/src/main/java/edu/northwestern/websail/tabel/unitTest/TestEmbedding.java
+++ table-entity-linker-test/src/main/java/edu/northwestern/websail/tabel/unitTest/TestEmbedding.java
@@ -13,14 +13,15 @@ public class TestEmbedding {
 
     static public void main(String[] args) {
         Embeddings embObj = new Embeddings();
-        HashMap<String, double[]> map = embObj.loadEmbedding();
+        embObj.loadEmbedding();
+        HashMap<String, double[]> map = embObj.embeddingVectorsMap_c;
         double[] d1 = map.get("d");
         double[] d2 = map.get("e");
         double res = 0.0;
         for (int i = 0; i < d1.length; i++) {
             res += d1[i] * d2[i];
         }
-        double res2 = embObj.calculateSimilarity("d", "e");
+        double res2 = embObj.calculateSimilarity_c("d", "e");
         System.out.println(res == res2);
     }
 }
